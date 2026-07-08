8019ad3fc41c70177bff88e8c06ae1708eee3a9a
Internee <intern@BarrettHodgson.com>
2026-07-08 11:12:27 +0500

    fixed migration bug

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
 
     from app import models
     return app
