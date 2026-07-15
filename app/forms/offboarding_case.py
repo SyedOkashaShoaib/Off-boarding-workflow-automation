c583f355eacf4ce3565a9dad353b62a99cdf54df
SyedOkashaShoaib <syedokashashoaib47@gmail.com>
2026-07-15 09:15:34 +0500

    update base.html

--- app/forms/offboarding_case.py
+++ app/forms/offboarding_case.py
@@ -6,7 +6,7 @@ class Case_Form(FlaskForm):
     emp_name = StringField(label='Employee name', validators=[DataRequired(message="Employee name is required")] )
     # emp_id = IntegerField('Employe')
     emp_desig = StringField(label="Employee designation", validators=[DataRequired(message='Employee Designation is required')])
-    emp_dep = StringField(label="EMployee department", validators=[DataRequired(message='Employee department is required')])
+    emp_dep = StringField(label="Employee department", validators=[DataRequired(message='Employee department is required')])
     last_date = DateField(label="Employee's last date of work", validators=[DataRequired(message='Last date of working si required')])
     emp_id = IntegerField(label='Employee id',validators=[DataRequired(message='Employee id is required')])
     line_manager = StringField(label="Line manager/HOD", validators=[DataRequired(message='Employee id is required')])
